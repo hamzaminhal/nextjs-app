746ddaa2ae2c86c0937d1a7084508ea7d95003c7
hamzaminhal <hamzaminhal2@gmail.com>
2025-11-08 11:40:22 +0500

    bg changed

--- src/app/page.js
+++ src/app/page.js
@@ -6,13 +6,13 @@ export default function Home() {
       <div className="flex items-center justify-center">
         <Link
           href={"/login"}
-          className="cursor-pointer m-5 bg-amber-900 text-amber-50 p-5 rounded-2xl"
+          className="cursor-pointer m-5 bg-teal-700 text-white p-5 rounded-2xl"
         >
           Login
         </Link>
         <Link
           href={"/signup"}
-          className="cursor-pointer m-5 bg-amber-900 text-amber-50 p-5 rounded-2xl"
+          className="cursor-pointer m-5 bg-teal-700 text-white p-5 rounded-2xl"
         >
           Sign Up
         </Link>
